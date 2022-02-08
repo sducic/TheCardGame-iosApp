d00e37b3416639e4836dc7cb20fbdeb22f22695a
sducic <ducic_stefan@hotmail.com>
2022-02-08 12:22:30 +0100

    Initial commit

---
+++ TheCardGame-iosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheCardGame-iosApp
+//
+//  Created by Stefan Ducic on 8.2.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
