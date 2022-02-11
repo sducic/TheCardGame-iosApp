f7cd4f73402d46109956905a824fa1a23820e266
sducic <ducic_stefan@hotmail.com>
2022-02-11 14:55:45 +0100

    delegate

--- TheCardGame-iosApp/ViewController.swift
+++ TheCardGame-iosApp/ViewController.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
     
     let model = CardModel()
     var cardsArray = [Card]()
@@ -18,6 +21,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         cardsArray = model.getCards()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        //return number of cards
+        return cardsArray.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // Get a cell
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        //Configure it
+        
+        
+        
+        //return it
+        return cell
     }
 
 
